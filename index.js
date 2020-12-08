8f9fd728c3b3376507ee935731126a9054a6bcab
Soldy <4786022+Soldy@users.noreply.github.com>
2020-12-08 12:36:44 +0000

    functon logic
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const sortBase = function(){
+    this.set = function (){
+
+    }
+    this.sort = function(){
+
+    }
+    this.add  = function (){
+
+    }
+    let list   =  {};
+    let sorted =  []; 
+    const swap = function(A,B){
+        let temp = sorted[A];
+        sorted[A] = sorted[B];
+        sorted[B] = temp;
+    }
+
+}
